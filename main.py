efb77c0e117fe32dcda27b29fa61b113aa692c6a
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2020-04-28 18:16:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,27 @@ import http.server
 import urllib.parse
 import json
 import time
+import os
+
+port = int(os.environ['METRICS_PORT']) if "METRICS_PORT" in os.environ else 8080
+disks_path = {}
+net_devices = {}
+
+for k, v in os.environ.items():
+    if k[0:13] == 'METRICS_DISK_':
+        disks_path[k[13:].lower()] = v
+    if k[0:12] == 'METRICS_NET_':
+        net_devices[k[12:].lower()] = v
+
+if not disks_path:
+    disks_path['root'] = '/'
+
+if not net_devices:
+    device = next(x for x in psutil.net_if_stats().keys() if x[0:3] == 'eth')
+    net_devices[device] = device
 
 def get_stats():
-    disk_root=psutil.disk_usage('/')
-    disk_external=psutil.disk_usage('/media/USB-DD')
+
     load = psutil.getloadavg()
     mem = psutil.virtual_memory()
     swap = psutil.swap_memory()
@@ -21,14 +38,6 @@ def get_stats():
     #disk_io = psutil.disk_io_counters()
 
     stats = {
-      'disk_root_used': disk_root.used,
-      'disk_root_free': disk_root.free,
-      'disk_root_used_pct': disk_root.percent,
-
-      'disk_external_used': disk_external.used,
-      'disk_external_free': disk_external.free,
-      'disk_external_used_pct': disk_external.percent,
-
       'load_1': load[0],
       'load_5': load[1],
       'load_15': load[2],
@@ -39,11 +48,18 @@ def get_stats():
 
       'swap_used': swap.used,
       'swap_free': swap.free,
-      'swap_used_pct': swap.percent,
-
-      'net_eth0_bytes_sent_s': round((net1['eth0'].bytes_sent - net0['eth0'].bytes_sent) / 5),
-      'net_eth0.bytes_recv_s': round((net1['eth0'].bytes_recv - net0['eth0'].bytes_recv) / 5)
+      'swap_used_pct': swap.percent
     }
+
+    for disk_name, disk_path in disks_path.items():
+        disk = psutil.disk_usage(disk_path)
+        stats['disk_' + disk_name + '_used'] = disk.used
+        stats['disk_' + disk_name + '_free'] = disk.free
+        stats['disk_' + disk_name + '_used_pct'] = disk.percent
+
+    for net_name, net_device in net_devices.items():
+        stats['net_' + net_name + '_bytes_sent_s'] = round((net1[net_device].bytes_sent - net0[net_device].bytes_sent) / 5)
+        stats['net_' + net_name + '_bytes_recv_s'] = round((net1[net_device].bytes_recv - net0[net_device].bytes_recv) / 5)
 
     return stats
 
@@ -71,9 +87,9 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             self.wfile.write(bytes(str(inst), 'utf8'))
             print('ERROR ' + str(inst))
 
-httpd = socketserver.TCPServer(('', 8734), Handler)
+httpd = socketserver.TCPServer(('', port), Handler)
 try:
-   print('Listening')
+   print('Listening on ' + str(port))
    httpd.serve_forever()
 except KeyboardInterrupt:
    pass
