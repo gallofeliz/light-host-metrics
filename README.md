a6c8e1e479ac2697fff09450c1c4ecd9dafdab8d
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2020-05-03 13:28:12 +0200

    Improved network

--- README.md
+++ README.md
@@ -2,8 +2,90 @@
 
 NEED HOST NETWORK FOR DOCKER
 
-`env METRICS_NET_ETH=wlp3s0 METRICS_DISK_ROOT=/ METRICS_DISK_EXTERNAL_DD=/media/drive METRICS_PORT=8888 python3 main.py`
+`env METRICS_NET_DEVICE_ETH=wlp3s0 METRICS_DISK_ROOT=/ METRICS_DISK_EXTERNAL_DD=/media/drive METRICS_PORT=8888 python3 main.py`
 
-`sudo docker run --net=host -v /media/drive:/media/drive:ro -e METRICS_NET_ETH=wlp3s0 -e METRICS_DISK_ROOT=/ -e METRICS_DISK_EXTERNAL_DD=/media/drive -e METRICS_PORT=8888 light-host-metrics-server` after build
+`sudo docker run --net=host -v /media/drive:/media/drive:ro -e METRICS_NET_DEVICE_ETH=wlp3s0 -e METRICS_DISK_ROOT=/ -e METRICS_DISK_EXTERNAL_DD=/media/drive -e METRICS_PORT=8888 light-host-metrics-server` after build
 
 Then, you can call http://localhost:8888 and use metric. For example with node-red, I call each 5 minutes this endpoint and push to influxdb. And then I can see in my grafana :)
+
+## Output
+
+`env METRICS_NET_DEVICE_wifi=wlp3s0 METRICS_NET_DEVICE_lo=lo METRICS_PORT=8080 METRICS_DISK_root=/ METRICS_NET_INTERVALS=1,5 python3 main.py`
+
+http://localhost:8080 :
+
+```
+{
+  "swap": {
+    "free": 33751560192,
+    "used": 0,
+    "usedPct": 0
+  },
+  "load": {
+    "1": 2.65,
+    "5": 3.54,
+    "15": 3.53
+  },
+  "net": {
+    "wifi": {
+      "1": {
+        "sentSpeed": 488,
+        "receivedSpeed": 770
+      },
+      "5": {
+        "sentSpeed": 1801,
+        "receivedSpeed": 21411
+      }
+    },
+    "lo": {
+      "1": {
+        "sentSpeed": 41,
+        "receivedSpeed": 41
+      },
+      "5": {
+        "sentSpeed": 65,
+        "receivedSpeed": 65
+      }
+    }
+  },
+  "disk": {
+    "root": {
+      "free": 120939520000,
+      "used": 86611247104,
+      "usedPct": 41.7
+    }
+  },
+  "mem": {
+    "free": 20786286592,
+    "used": 5835284480,
+    "usedPct": 21.1
+  }
+}
+```
+
+http://localhost:8080/flat :
+
+```
+{
+  "disk_root_used_pct": 41.7,
+  "mem_free": 20843929600,
+  "net_wifi_5_sent_speed": 2179,
+  "net_wifi_1_sent_speed": 2563,
+  "net_lo_1_received_speed": 88,
+  "disk_root_used": 86611058688,
+  "load_1": 3.09,
+  "net_lo_5_received_speed": 70,
+  "disk_root_free": 120939708416,
+  "load_5": 3.54,
+  "mem_used_pct": 20.9,
+  "net_lo_5_sent_speed": 70,
+  "net_lo_1_sent_speed": 88,
+  "net_wifi_5_received_speed": 23795,
+  "swap_used_pct": 0,
+  "net_wifi_1_received_speed": 22674,
+  "load_15": 3.53,
+  "mem_used": 5770801152,
+  "swap_free": 33751560192,
+  "swap_used": 0
+}
+```
--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python
 
-import psutil, socketserver, http.server, urllib.parse, json, time, os, re
+import psutil, socketserver, http.server, urllib.parse, json, time, os, re, threading, datetime
 
 port = int(os.environ['METRICS_PORT']) if "METRICS_PORT" in os.environ else 8080
 disks_path = {}
 net_devices = {}
 
+net_collect=[]
+net_collect_interval=30
+net_intervals_minutes=[0, 1, 5, 15]
+max_net_interval_minutes=max(net_intervals_minutes)
+net_zero_interval = 1
+
 for k, v in os.environ.items():
     if k[0:13] == 'METRICS_DISK_':
         disks_path[k[13:].lower()] = v
-    if k[0:12] == 'METRICS_NET_':
-        net_devices[k[12:].lower()] = v
+    if k[0:19] == 'METRICS_NET_DEVICE_':
+        net_devices[k[19:].lower()] = v
+    if k == 'METRICS_NET_INTERVALS':
+        net_intervals_minutes=list(map(int, v.split(',')))
+    if k == 'METRICS_NET_COLLECT_INTERVAL':
+        net_collect_interval=int(v)
+    if k == 'METRICS_NET_ZERO_INTERVAL':
+        net_zero_interval=int(v)
 
 if not disks_path:
     disks_path['root'] = '/'
@@ -33,14 +45,29 @@ def flatten_dict(d):
 
     return dict(items())
 
+def collect_network(sec):
+    global net_collect
+    threading.Timer(sec, lambda: collect_network(sec) ).start()
+    max_net_interval_minutes_datetime = datetime.datetime.now() - datetime.timedelta(minutes=max_net_interval_minutes)
+    # Can be optimized, no need to iterate on all the list and maybe no need to create new one
+    net_collect = list(filter(lambda x: x['datetime'] >= max_net_interval_minutes_datetime, net_collect))
+    net_collect.append({
+        "datetime": datetime.datetime.now(),
+        "network": psutil.net_io_counters(pernic=True)
+    })
+
+def get_net(interval_minutes):
+    global net_collect
+    datetime_wanted = datetime.datetime.now() - datetime.timedelta(minutes=interval_minutes)
+    differences = list(map(lambda x: abs((x['datetime'] - datetime_wanted).total_seconds()), net_collect))
+    min_difference=min(differences)
+    return net_collect[differences.index(min_difference)]
+
 def get_stats():
 
     load = psutil.getloadavg()
     mem = psutil.virtual_memory()
     swap = psutil.swap_memory()
-    net0 = psutil.net_io_counters(pernic=True)
-    time.sleep(1)
-    net1 = psutil.net_io_counters(pernic=True)
 
     #disk_io = psutil.disk_io_counters()
 
@@ -75,11 +102,28 @@ def get_stats():
             'usedPct': disk.percent
         }
 
+    if 0 in net_intervals_minutes:
+        net0 = psutil.net_io_counters(pernic=True)
+        time.sleep(net_zero_interval)
+    net_now = psutil.net_io_counters(pernic=True)
+
     for net_name, net_device in net_devices.items():
-        stats['net'][net_name] = {
-            'sentSpeed': round((net1[net_device].bytes_sent - net0[net_device].bytes_sent) / 1),
-            'receivedSpeed': round((net1[net_device].bytes_recv - net0[net_device].bytes_recv) / 1)
-        }
+        stats['net'][net_name] = {}
+        for net_interval in net_intervals_minutes:
+            if net_interval == 0:
+                stats['net'][net_name][str(net_interval)] = {
+                    'sentSpeed': round((net_now[net_device].bytes_sent - net0[net_device].bytes_sent) / net_zero_interval),
+                    'receivedSpeed': round((net_now[net_device].bytes_recv - net0[net_device].bytes_recv) / net_zero_interval)
+                }
+            else:
+                datetime_now=datetime.datetime.now()
+                found_net=get_net(net_interval)
+                real_interval=(datetime_now - found_net['datetime']).total_seconds()
+
+                stats['net'][net_name][str(net_interval)] = {
+                    'sentSpeed': round((net_now[net_device].bytes_sent - found_net['network'][net_device].bytes_sent) / real_interval),
+                    'receivedSpeed': round((net_now[net_device].bytes_recv - found_net['network'][net_device].bytes_recv) / real_interval)
+                }
 
     return stats
 
@@ -107,6 +151,8 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             self.wfile.write(bytes(str(inst), 'utf8'))
             print('ERROR ' + str(inst))
 
+if max_net_interval_minutes > 0:
+    collect_network(net_collect_interval)
 httpd = socketserver.TCPServer(('', port), Handler)
 try:
    print('Listening on ' + str(port))
